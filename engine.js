987221170fa4ef445b358648f47130e8652d84ca
trixky <mathis.bois@outlook.fr>
2020-12-06 12:03:53 +0100

    finish all parser with object generation for facts

--- engine.js
+++ engine.js
@@ -1,3 +1,8 @@
 const parser = require('./src/parser');
 const path = __dirname + '/' + process.argv[2];
-parser(process.cwd() + '/' + process.argv[2]);
+const { parsed, facts } = parser(process.cwd() + '/' + process.argv[2]);
+
+console.log("\n\n------------------------ PARSED\n\n")
+console.log(JSON.stringify(parsed, null, 4));
+console.log("\n\n------------------------ FACTS\n\n")
+console.log(facts);
--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,71 @@
 const fs = require('fs');
 const peg = require('pegjs');
 
-module.exports = path => {
+const from_expression = (expression, facts) => {
+	if (expression.type === '!') {
+		from_expression(expression.value, facts);
+	}
+	else if (expression.type === 'v') {
+		if (!facts[expression.value]) {
+			facts[expression.value] = { label: expression.value, value: null };
+		}
+		expression.value = facts[expression.value];
+	}
+	else {
+		from_expression(expression.left, facts);
+		from_expression(expression.right, facts);
+	}
+};
+
+const from_rules = (rules, facts) => {
+	rules.forEach(rule => {
+		from_expression(rule.needed, facts);
+		from_expression(rule.given, facts);
+	});
+};
+
+const from_initial_facts = (initial_facts, facts) => {
+	initial_facts.forEach((initial_fact, index) => {
+		if (!facts[initial_fact]) {
+			facts[initial_fact] = { label: initial_fact};
+		}
+		facts[initial_fact].value = true;
+		initial_facts[index] = facts[initial_fact];
+	})
+};
+
+const from_queries = (queries, facts) => {
+	queries.forEach((querie, index) => {
+		if (!facts[querie]) {
+			facts[querie] = { label: querie, value: null };
+		}
+		queries[index] = facts[querie];
+	})
+};
+
+const generate_facts = parsed => {
+	const facts = {};
+
+	from_rules(parsed.rules, facts);
+	from_initial_facts(parsed.initial_facts, facts);
+	from_queries(parsed.queries, facts);
+
+	return facts;
+};
+
+const parser = path => {
 	const grammar = fs.readFileSync(__dirname + '/grammar.peg', 'utf-8');
 	const input = fs.readFileSync(path, 'utf-8');
 	const parser = peg.generate(grammar);
-	
+
 	try {
 		const parsed = parser.parse(input);
-		console.log(JSON.stringify(parsed, null, 4));
+		const facts = generate_facts(parsed);
+
+		return {parsed, facts};
 	} catch (err) {
-		console.log(err)
+		return null;
 	}
-}
+};
+
+module.exports = parser;

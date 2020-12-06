9f1c51b10b7fdd5567db55df23d8508c2485f408
trixky <mathis.bois@outlook.fr>
2020-12-06 15:17:58 +0100

    catch parsing error

--- engine.js
+++ engine.js
@@ -1,14 +1,18 @@
 const parser = require('./src/parser');
 const query_solution = require('./src/solver');
 const path = __dirname + '/' + process.argv[2];
-const { parsed, facts } = parser(process.cwd() + '/' + process.argv[2]);
+try {
+	const { parsed, facts } = parser(process.cwd() + '/' + process.argv[2]);
 
-console.log("\n\n------------------------ PARSED\n\n")
-console.log(JSON.stringify(parsed, null, 4));
-console.log("\n\n------------------------ FACTS\n\n")
-console.log(facts);
+	console.log("\n\n------------------------ PARSED\n\n")
+	console.log(JSON.stringify(parsed, null, 4));
+	console.log("\n\n------------------------ FACTS\n\n")
+	console.log(facts);
 
-console.log("\n\n------------------------ RESULTS\n\n");
-parsed.queries.forEach((query) => {
-	console.log(query.label+ " - " + query_solution(query, parsed.rules, facts));
-});
+	console.log("\n\n------------------------ RESULTS\n\n");
+	parsed.queries.forEach((query) => {
+		console.log(query.label + " - " + query_solution(query, parsed.rules, facts));
+	});
+} catch (err) {
+	console.log("\n\n------------------------ PARSING ERROR\n\n");
+}

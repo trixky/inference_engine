089b239e70c598d19262a829306d5ec342024216
trixky <mathis.bois@outlook.fr>
2020-12-06 11:12:14 +0100

    finish to pass input in engine by argv

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const peg = require('pegjs');
 
-module.exports = file => {
-	console.log(__dirname)
-	const grammar = fs.readFileSync('./src/grammar.peg', 'utf-8');
-	const path = file;
-	console.log(path)
+module.exports = path => {
+	const grammar = fs.readFileSync(__dirname + '/grammar.peg', 'utf-8');
 	const input = fs.readFileSync(path, 'utf-8');
 	const parser = peg.generate(grammar);
+	
 	try {
 		const parsed = parser.parse(input);
 		console.log(JSON.stringify(parsed, null, 4));
